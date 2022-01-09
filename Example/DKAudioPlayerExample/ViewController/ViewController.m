9211198a6ca9550ac15c1b32a740edba905c215b
Denis Kutlubaev <kutlubaev.denis@gmail.com>
2022-01-09 14:34:10 +0300

    Improve the placement of DKAudioPlayer on example ViewController

--- Example/DKAudioPlayerExample/ViewController/ViewController.m
+++ Example/DKAudioPlayerExample/ViewController/ViewController.m
@@ -7,67 +7,46 @@
 #import "ViewController.h"
 #import <DKAudioPlayer/DKAudioPlayer.h>
 
-
 @interface ViewController ()
 
 @property (strong, nonatomic) DKAudioPlayer *audioPlayer;
-
-- (IBAction)showHideClicked:(id)sender;
+@property (weak, nonatomic) IBOutlet UIView *audioPlayerContainerView;
 
 @end
 
-
 @implementation ViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    
-    if ( self ) {
-        
+
+    if (self) {
         self.tabBarItem.title = @"View";
         self.tabBarItem.image = [UIImage imageNamed:@"42-photos"];
-        
     }
-    
+
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     
     NSString *audioFilePath = [[NSBundle mainBundle] pathForResource:@"sample.mp3" ofType:nil];
-    
-    if ( audioFilePath ) {
-        
-        // The width of a player is equal to the width of a parent view
-        self.audioPlayer = [[DKAudioPlayer alloc] initWithAudioFilePath:audioFilePath width:self.view.frame.size.width height:0];
+    if (!audioFilePath) { return; }
 
-        // You can set any background color to the background view of the player
-        //self.audioPlayer.backgroundViewColor = [UIColor clearColor];
-        
-        // Setting the origin of an audio player
-        CGRect frame = self.audioPlayer.frame;
-        frame.origin = CGPointMake(0, self.view.frame.size.height - self.tabBarController.tabBar.frame.size.height);
-        self.audioPlayer.frame = frame;
-        
-        // Adding player on a view
-        [self.view addSubview:self.audioPlayer];
-    }
+    // The width of a player is equal to the width of a parent view
+    self.audioPlayer = [[DKAudioPlayer alloc] initWithAudioFilePath:audioFilePath
+                                                              width:self.audioPlayerContainerView.frame.size.width
+                                                             height:self.audioPlayerContainerView.frame.size.height];
+
+    // You can set any background color to the background view of the player
+    // self.audioPlayer.backgroundViewColor = [UIColor clearColor];
+
+    // Adding player on a view
+    self.audioPlayer.frame = self.audioPlayerContainerView.bounds;
+    [self.audioPlayerContainerView addSubview:self.audioPlayer];
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-    
-    if (! self.audioPlayer.isVisible) {
-        [self.audioPlayer showAnimated:YES];
-    }
-}
-
-- (IBAction)showHideClicked:(id)sender
-{
+- (IBAction)showHideButtonTapped:(id)sender {
     self.audioPlayer.hidden = !self.audioPlayer.isHidden;
 }
 
